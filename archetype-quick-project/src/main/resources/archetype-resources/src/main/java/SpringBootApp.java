667505f7f8346f4c9b69456c252ade0dfc9d2238
zongf0504 <15737250504@139.com>
2019-12-15 15:23:39 +0800

    修改启动类名

--- archetype-quick-project/src/main/resources/archetype-resources/src/main/java/SpringBootApp.java
+++ archetype-quick-project/src/main/resources/archetype-resources/src/main/java/SpringBootApp.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        SpringApplication.run(SpringBootApp.class, args);
     }
 }
