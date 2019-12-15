094af0156c20db3d3f5780fd2c7a58ad14f5efbb
zongf0504 <15737250504@139.com>
2019-12-15 16:33:23 +0800

    新增文档说明

--- archetype-quick-project/src/main/resources/archetype-resources/src/test/java/SpringBootAppTest.java
+++ archetype-quick-project/src/main/resources/archetype-resources/src/test/java/SpringBootAppTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CodeGeneratorTest {
+public class SpringBootAppTest {
 
     @Test
     public void contextLoads() {
